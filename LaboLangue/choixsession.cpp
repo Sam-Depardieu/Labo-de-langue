d5545a23727feaf783429e2458c86a34a59b3464
Sam DEPARDIEU <samdepardieu@hotmail.com>
2025-05-06 22:57:40 +0200

    Premier jet interface choix session

--- LaboLangue/choixsession.cpp
+++ LaboLangue/choixsession.cpp
@@ -1,14 +1,57 @@
 #include "choixsession.h"
 #include "ui_choixsession.h"
 
-choixSession::choixSession(QWidget *parent)
-    : QWidget(parent)
+choixSession::choixSession(MainWindow* parentWindow)
+    : mainWindow(parentWindow)
     , ui(new Ui::choixSession)
 {
     ui->setupUi(this);
+
+    model = new QStandardItemModel(this);
+    columnView->setModel(model);
+
+    // Charger les dossiers de sessions
+    basePath = R"(\\CIEL-T171-05\Activites\)";              //à changer
+    QDir dir(basePath);
+    QStringList sessionDirs = dir.entryList(QDir::Dirs | QDir::NoDotAndDotDot);
+
+    for (const QString &folder : sessionDirs) {
+        listWidget->addItem(folder);
+    }
 }
 
 choixSession::~choixSession()
 {
     delete ui;
 }
+
+void choixSession::on_listeSession_itemDoubleClicked(QListWidgetItem *item)
+{
+    QString selectedPath = basePath + item->text() + "/config.labo";
+    QFile file(selectedPath);
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "❌ Impossible d'ouvrir" << selectedPath;
+        model->clear();
+        model->appendRow(new QStandardItem("Erreur : Impossible d’ouvrir le fichier."));
+        return;
+    }
+
+    QByteArray data = file.readAll();
+    file.close();
+
+    QJsonParseError error;
+    QJsonDocument doc = QJsonDocument::fromJson(data, &error);
+    if (error.error != QJsonParseError::NoError || !doc.isObject()) {
+        model->clear();
+        model->appendRow(new QStandardItem("Erreur : JSON invalide."));
+        return;
+    }
+
+    QJsonObject obj = doc.object();
+    model->clear();
+    for (const QString &key : obj.keys()) {
+        QStandardItem *item = new QStandardItem(key + " : " + obj[key].toString());
+        model->appendRow(item);
+    }
+}
+
--- LaboLangue/choixsession.h
+++ LaboLangue/choixsession.h
@@ -1,22 +1,43 @@
 #ifndef CHOIXSESSION_H
 #define CHOIXSESSION_H
 
-#include <QWidget>
+#include <QMainWindow>
+#include <QListWidget>
+#include <QColumnView>
+#include <QSplitter>
+#include <QDir>
+#include <QFile>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QStandardItemModel>
+#include <QVBoxLayout>
+#include <QDebug>
 
 namespace Ui {
 class choixSession;
 }
+class MainWindow;
 
 class choixSession : public QWidget
 {
     Q_OBJECT
 
 public:
-    explicit choixSession(QWidget *parent = nullptr);
+    explicit choixSession(MainWindow* parentWindow);
+
     ~choixSession();
 
+private slots:
+    void on_listeSession_itemDoubleClicked(QListWidgetItem *item);
+
 private:
+    MainWindow* mainWindow;
     Ui::choixSession *ui;
+
+    QListWidget *listWidget;
+    QColumnView *columnView;
+    QStandardItemModel *model;
+    QString basePath;
 };
 
 #endif // CHOIXSESSION_H
